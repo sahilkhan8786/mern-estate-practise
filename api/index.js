018d3779fbbc90909ba115f39e1657b02be16ccd
sahilkhan8786 <official.khansahil8786@gmail.com>
2023-11-24 11:57:20 +0530

    first commit

--- api/index.js
+++ api/index.js
@@ -1,6 +1,18 @@
 import express from 'express'
+import dotenv from 'dotenv'
+import mongoose from 'mongoose'
 
 const app = express();
+dotenv.config()
+
+
+mongoose
+    .connect(process.env.MONGO_URI)
+    .then(() => {
+        console.log("Connected to MongoDB")
+    }).catch(err => {
+        console.log(err)
+    })
 
 
 const PORT = 3000;
